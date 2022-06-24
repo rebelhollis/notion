6760ddc929aace3de4d469c0575733eecf5be8c8
rebelhollis <108132210+rebelhollis@users.noreply.github.com>
2022-06-24 17:33:56 +0100

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -25,7 +25,8 @@ function light() {
 	document.getElementById('weather').setAttribute('data-theme', 'pure');
 	document.getElementById('weather').removeAttribute('data-basecolor');
 	document.getElementById('weather').setAttribute('data-textcolor', '#37352f');
-	document.getElementById('weather').removeAttribute('data-cloudfill');
+	document.getElementById('weather').removeAttribute('data-cloudfill', '#f3f3f3');
+	document.getElementById('weather').setAttribute('data-suncolor', '#ffae00');
 	script(document, 'script', 'weatherwidget-io-js');
 }
 function dark() {
@@ -34,11 +35,12 @@ function dark() {
 	document.getElementById('weather').setAttribute('data-theme', 'gray');
 	document.getElementById('weather').setAttribute('data-basecolor', '#191919');
 	document.getElementById('weather').removeAttribute('data-textcolor');
-	document.getElementById('weather').setAttribute('data-cloudfill', '#191919');
+	document.getElementById('weather').setAttribute('data-cloudfill', '#f3f3f3');
+	document.getElementById('weather').setAttribute('data-suncolor', '#ffae00');
 	script(document, 'script', 'weatherwidget-io-js');
 }
 let currentTheme = localStorage.getItem('data-theme');
-if (currentTheme == 'pure') {
+if (currentTheme == 'light') {
 	light();
 } else {
 	dark();
