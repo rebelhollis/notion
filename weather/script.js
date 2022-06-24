6c1494779077e05c18a75f9a878dc1ea34e4f48b
rebelhollis <108132210+rebelhollis@users.noreply.github.com>
2022-06-24 17:29:56 +0100

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -48,22 +48,22 @@ if (currentTheme == 'pure') {
 // DYNAMIC THEME SETTINGS BASED ON OS PREFERENCE
 
 function light() {
-	document.documentElement.setAttribute('data-theme', 'pure');
-	document.getElementById('weather').setAttribute('data-theme', 'pure');
+	document.documentElement.setAttribute('data-theme', 'light');
+	document.getElementById('weather').setAttribute('data-theme', 'light');
 	document.getElementById('weather').removeAttribute('data-basecolor');
-	document.getElementById('weather').setAttribute('data-textcolor', '#37352f');
+	document.getElementById('weather').setAttribute('data-textcolor', '#600ef9');
 	document.getElementById('weather').removeAttribute('data-cloudfill');
-	document.getElementById('weather').setAttribute('data-suncolor', '#F58f70');
+	document.getElementById('weather').setAttribute('data-suncolor', '#ffae00');
 	script(document, 'script', 'weatherwidget-io-js');
 }
 
 function dark() {
-	document.documentElement.setAttribute('data-theme', 'gray');
-	document.getElementById('weather').setAttribute('data-theme', 'gray');
+	document.documentElement.setAttribute('data-theme', 'dark');
+	document.getElementById('weather').setAttribute('data-theme', 'dark');
 	document.getElementById('weather').setAttribute('data-basecolor', '#191919');
-	document.getElementById('weather').removeAttribute('data-textcolor');
+	document.getElementById('weather').removeAttribute('data-textcolor', '#ff1c8d');
 	document.getElementById('weather').setAttribute('data-cloudfill', '#191919');
-	document.getElementById('weather').setAttribute('data-suncolor', '#F58f70');
+	document.getElementById('weather').setAttribute('data-suncolor', '#ffae00');
 	script(document, 'script', 'weatherwidget-io-js');
 }
 
